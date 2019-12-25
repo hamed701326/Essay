038f534aa2060ab6c75812568cc7e6e547ddaf35
hamed701326 <sadeghi0hamed@gmail.com>
2019-12-25 14:16:51 +0300

    configuration is finished , we created User Entity

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,6 +1,9 @@
 import ir.science.essay.core.configuration.hibernate.HibernateUtil;
+import ir.science.essay.entities.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
+import java.util.Date;
 
 public class MainApp {
     public static void main(String[] args) {
@@ -8,5 +11,21 @@ public class MainApp {
         SessionFactory sessionFactory= HibernateUtil.getSessionFactory();
         //create session
         Session session=sessionFactory.openSession();
+        // start transaction
+        session.beginTransaction();
+        //----------------------------------
+        //save
+        User user=new User("asgharmohammadi","607",setDate(1996L,5L,2L),"607");
+        int id=(int) session.save(user);
+        System.out.printf("User with this %d inserted into table\n",id);
+
+        //----------------------
+        //commit transaction
+        session.getTransaction().commit();
+        session.close();
+    }
+    public static Date setDate(Long years,Long month,Long days){
+        long timeStamp=((years-1970)*365+(month-1)*30+(days-1))*24*3600*1000;
+        return new Date(timeStamp);
     }
 }
