769bf083517bd89c3f188d4537f5fa3dd31b241e
hamed701326 <sadeghi0hamed@gmail.com>
2019-12-25 15:40:56 +0300

    Category finished

--- src/main/java/ir/science/essay/entities/Category.java
+++ src/main/java/ir/science/essay/entities/Category.java
@@ -1,4 +1,18 @@
 package ir.science.essay.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private int id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private  String description;
 }
